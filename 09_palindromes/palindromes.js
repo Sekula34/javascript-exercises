80a32d00dd02c2c3f1c36f7b76fc9874e119740a
Sekula34 <filip.seles@gmail.com>
2025-02-01 15:42:52 +0100

    feat: solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
 
+function remove(str) {
+    return str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '');
+}
+
+
+/**
+ * 
+ * @param {String} str 
+ */
+const palindromes = function (str) {
+	
+	let trimmed = str.trim(); 
+	trimmed = trimmed.toLowerCase(); 
+	trimmed = remove(trimmed);
+	
+	let reverse = trimmed.split('').reverse().join('');
+	if(trimmed == reverse)
+		return true;
+	return false;
 };
 
 // Do not edit below this line
