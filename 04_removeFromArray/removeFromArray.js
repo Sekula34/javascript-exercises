43c129147c71ca299533af62ca1f03b86b15c444
Sekula34 <filip.seles@gmail.com>
2025-02-01 13:10:01 +0100

    feat: solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+/**
+ * 
+ * @param {Array} array 
+ * @param {any} other 
+ * @returns 
+ */
+const removeFromArray = function(array) {
+	// if(Array.isArray(array) === false)
+	// 	console.error("Not array");
+	// console.log(array);
+	for(let i = 1; i < arguments.length; i++)
+	{
+		let otherIndex = array.indexOf(arguments[i]);
+		while(otherIndex != -1)
+		{
+			array.splice(otherIndex, 1);
+			otherIndex = array.indexOf(arguments[i]);
+		}
+	}
+	return array;
 };
 
 // Do not edit below this line
