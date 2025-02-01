f9cbeb19274bb3688409c930aea35ff30056a3bb
Sekula34 <filip.seles@gmail.com>
2025-02-01 14:22:58 +0100

    feat: solve ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+	let sum = 0;
+	if(Number.isInteger(startNumber) == false || Number.isInteger(endNumber) == false || startNumber < 0 || endNumber < 0)
+		return "ERROR";
+	if(startNumber > endNumber)
+	{
+		const temp = startNumber;
+		startNumber = endNumber;
+		endNumber = temp;
+	}
+	for(let i = startNumber; i<=endNumber; i++)
+	{
+		sum += i;
+	}
+	return sum;
 
 };
 
