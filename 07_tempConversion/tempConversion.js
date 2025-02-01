ab781a84cdd09523446dc429bc957403e8172703
Sekula34 <filip.seles@gmail.com>
2025-02-01 14:49:31 +0100

    feat: solve ex07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * 5 / 9;
+
+  let round = celsius.toFixed(1);
+  return round = Number(round);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = 9/5*celsius + 32;
+  let round = fahrenheit.toFixed(1);
+  return round = Number(round);
 };
 
 // Do not edit below this line
